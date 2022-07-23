ef78bf6b3e32f790f87de7efa14a9695b4e75b4b
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-07-24 02:45:07 +0700

    update about references

--- app/src/main/java/com/andrew/simpleandroidapp/About.kt
+++ app/src/main/java/com/andrew/simpleandroidapp/About.kt
@@ -17,33 +17,65 @@ class About : AppCompatActivity(), View.OnClickListener {
         actionBar!!.title = "About"
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        val tvEmail: TextView   = findViewById(R.id.tv_url_email)
-        val btnLinkedIn: Button = findViewById(R.id.btn_url_linkedin)
-        val btnGitHub:Button    = findViewById(R.id.btn_url_github)
+        val tvEmail:         TextView = findViewById(R.id.tv_url_email)
+        val btnLinkedIn:     Button   = findViewById(R.id.btn_url_linkedin)
+        val btnGitHub:       Button   = findViewById(R.id.btn_url_github)
+        val tvDicoding:      TextView = findViewById(R.id.tv_url_dicoding)
+        val tvYouTube:       TextView = findViewById(R.id.tv_url_youtube)
+        val tvStackOverflow: TextView = findViewById(R.id.tv_url_stackoverflow)
+        val tvDBpedia:       TextView = findViewById(R.id.tv_url_dbpedia)
 
         tvEmail.setOnClickListener(this)
         btnLinkedIn.setOnClickListener(this)
         btnGitHub.setOnClickListener(this)
+        tvDicoding.setOnClickListener(this)
+        tvYouTube.setOnClickListener(this)
+        tvStackOverflow.setOnClickListener(this)
+        tvDBpedia.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.tv_url_email -> {
                 val link = "mailto:andrewbjamesie@yahoo.com"
-                val linkedInIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
-                startActivity(linkedInIntent)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
             }
 
             R.id.btn_url_linkedin -> {
                 val link = "https://www.linkedin.com/in/andrewbenedictusjamesie"
-                val linkedInIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
-                startActivity(linkedInIntent)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
             }
 
             R.id.btn_url_github -> {
                 val link = "https://github.com/aNdr3W03/SimpleAndroidApp"
-                val gitHubIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
-                startActivity(gitHubIntent)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
+            }
+
+            R.id.tv_url_dicoding -> {
+                val link = "https://www.dicoding.com/academies/51"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
+            }
+
+            R.id.tv_url_youtube -> {
+                val link = "https://www.youtube.com"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
+            }
+
+            R.id.tv_url_stackoverflow -> {
+                val link = "https://stackoverflow.com"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
+            }
+
+            R.id.tv_url_dbpedia -> {
+                val link = "https://www.dbpedia.org"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                startActivity(intent)
             }
         }
     }
